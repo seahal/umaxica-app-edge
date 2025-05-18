718bff7cabe4cb201d26d6e1444fd32042591c7c
seahal <seahal@gmail.com>
2025-05-19 01:58:06 +0900

    [misc] ..

--- src/route/world.tsx
+++ src/route/world.tsx
@@ -4,6 +4,8 @@ const app = new Hono();
 
 app.get("*", (c) => {
 	console.log(c.req.url);
+	// FIXME: This is a temporary solution
+	//        You know that this is not a good solution.
 	if (c.req.url.match(/umaxica.com/)) {
 		return c.redirect("https://jp.umaxica.com", 301);
 	} else if (c.req.url.match(/umaxica.org/)) {
