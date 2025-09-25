c6db13145e46e2797cad1f4e9396a6d69cbee098
seahal <seahal.com@gmail.com>
2025-09-26 05:52:05 +0900

    [update] .......

--- app/src/root.tsx
+++ app/src/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<body>
 				{children}
 				<ScrollRestoration />
-				<Scripts nonce={nonce} />
+				<Scripts />
 			</body>
 		</html>
 	);
