aa1e3886679b90652b17a44bf8cf2ec432e9b1e7
seahal <seahal.com@gmail.com>
2025-10-06 11:22:43 +0000

    Fix assertion in smoke test to use correct matcher

--- app/test/welcome.component.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it, test } from "bun:test";
-import { render, screen } from "@testing-library/react";
-import { Welcome } from "../src/welcome/welcome";
-
-test("LoginForm renders error messages", async () => {
-	const USER_MESSAGE = "Username is required";
-	const PASSWORD_MESSAGE = "Password is required";
-});
