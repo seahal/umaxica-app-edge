bdcb8923a685a21ff65e8ce29bbd23c80fe10fbe
seahal <seahal.com@gmail.com>
2025-10-09 21:09:42 +0900

    [misc] .....

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer({ codeName = "" }: FooterProps) {
 	return (
 		<footer style={{ textAlign: "center" }} className="p-6">
 			<small>
-				© {new Date().getFullYear()} {codeName}.
+				© {new Date().getFullYear()} {codeName}
 			</small>
 		</footer>
 	);
