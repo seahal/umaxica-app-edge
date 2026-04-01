53ccb5912c9f0fa938de34c4e3b62a5fa87e496d
seahal <m.shiihara@gmail.com>
2026-04-01 09:07:27 +0000

    [learning] nextjs.

--- app/core/next.config.ts
+++ app/core/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from 'next';
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 
 const nextConfig: NextConfig = {};
 
 export default nextConfig;
-
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 void initOpenNextCloudflareForDev();
